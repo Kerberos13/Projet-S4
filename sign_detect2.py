3baa7f7e07e18260a169496eed0cc593dc06bb4b
Benoît Porteboeuf <porteboeuf.benoit@gmail.com>
2016-05-02 22:18:31 +0200

    BW and HSV functions moved to the tools module

--- sign_detect2.py
+++ sign_detect2.py
@@ -8,7 +8,7 @@
 import sys, os, numpy
 from PIL import Image
 from math import floor
-
+from tools import *
 
 
 # This function displays text on a console like interface if the GUI is started
@@ -49,44 +49,6 @@ def closef(picture) :
         sys.exit()
     return
 
-
-
-
-
-# This function makes it Black and White
-
-def BW(picture) :
-
-    if isinstance(picture,Image.Image) : # We verify that we effectively are working on an Image object
-        picture = picture.convert('L')
-        picture = numpy.asarray(picture)
-        h,v = picture.shape[0],picture.shape[1]
-        #picture = (picture[0:h,0:v,0]*1+picture[0:h,0:v,1]*1+picture[0:h,0:v,2]*4)/6
-        return picture
-
-    else :
-        print("Wrong Type - Fatal Error.\n")
-        sys.exit()
-        return
-
-
-
-# This function makes an RGB picture into a Hue Saturation Value Picture
-
-def HSV(picture) :
-    
-    if isinstance(picture,Image.Image) : 
-        picture = picture.convert('HSV')
-        picture = numpy.asarray(picture)
-        h,v = picture.shape[0],picture.shape[1]
-        picture = picture[0:h,0:v,0]
-
-        return picture
-
-    else :
-        print("Wrong Type - Fatal Error.\n")
-        sys.exit()
-        return
 
 
 
