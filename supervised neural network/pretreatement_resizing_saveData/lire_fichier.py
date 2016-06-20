091a1467fbee06b4c45b6b10420bec5e6c8d76b1
Benoît Porteboeuf <porteboeuf.benoit@gmail.com>
2016-06-20 23:23:09 +0200

    Cleaning among files

--- supervised neural network/pretreatement_resizing_saveData/lire_fichier.py
+++
@@ -1,8 +0,0 @@
-__author__ = 'yxu01'
-import os
-def lire_fichier(NomfichierS) :
-    NomfichierD =[]
-    for dossier,sous_dossier,fichiers in os.walk(NomfichierS):
-        for fichier in fichiers :
-            NomfichierD.append([dossier,fichier])
-    return NomfichierD
